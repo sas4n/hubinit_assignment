aae7822708fdb16a6e65584b17abfe6189e39946
Rahim Pormah <sas4n.porm4h@gmail.com>
2024-04-01 18:47:24 +0200

    format_suffix-patternd added

--- back-end/Api/urls.py
+++ back-end/Api/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-
+from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
     path('blogs/', views.BlogList.as_view()),
     path('blogs/<int:pk>/', views.BlogDetail.as_view()),
 ]
+
+#this will make us choose our desired format, for instance, if the url ends with .json the receiving data would be in json format.
+urlpatterns = format_suffix_patterns(urlpatterns)
