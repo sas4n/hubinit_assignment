916cb0c0d65daf55f0c7efc5e07203b0b40d9c32
Rahim Pormah <sas4n.porm4h@gmail.com>
2024-04-01 13:58:34 +0200

    admin updated and Model blog renamed to Blog

--- back-end/Api/admin.py
+++ back-end/Api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Blog
 
 # Register your models here.
+@admin.register(Blog)
+class BlogsAdmin(admin.Admin):
+    list_display = ['blog_title', 'author_name']
--- back-end/Api/models.py
+++ back-end/Api/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-class blog(models.Model):
+class Blog(models.Model):
     blog_title = models.CharField(max_length=50, blank=False)
     author_name = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blogs")
     blog_text = models.TextField(blank=False)
