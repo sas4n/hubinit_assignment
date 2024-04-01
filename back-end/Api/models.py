3c9707e5e2280677c83f3de99585b2a6d18208da
Rahim Pormah <sas4n.porm4h@gmail.com>
2024-04-01 13:30:31 +0200

    Model added

--- back-end/Api/models.py
+++ back-end/Api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class blog(models.Model):
+    blog_title = models.CharField(max_length=50)
+    author_name = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blogs")
+    blog_text = models.TextField()
+    ACTIVE = "ACTIVE"
+    DRAFT = "DRAFT"
+    STATUS = {
+        ACTIVE: "Active",
+        DRAFT: "Draft",
+    }
+    blog_status = models.CharField(max_length=6, choices=STATUS)
+
