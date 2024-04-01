7ab4e753745640ce92b868705f21085ac093f0fb
Rahim Pormah <sas4n.porm4h@gmail.com>
2024-04-01 16:33:38 +0200

    views and urls updated

--- back-end/Api/urls.py
+++ back-end/Api/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path('blogs/', views.BlogList.as_view()),
+    path('blogs/<int:pk>/', views.BlogDetail.as_view()),
 ]
--- back-end/Api/views.py
+++ back-end/Api/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from rest_framework import generics
+from .models import Blog
+from .serializers import BlogSerializer
 
 # Create your views here.
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the Api index.")
+class BlogList(generics.ListCreateAPIView):
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author_name=self.request.user)
+
+class BlogDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
